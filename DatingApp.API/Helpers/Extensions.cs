24a3a7a78e0eeed208257aba21eb2558aeb8e01b
luke.oregan <luke.oregan@veritas.com>
2019-09-01 20:35:00 +0100

    Creating Users controller, adding AutoMapper

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Http;
-
+using System;
 namespace DatingApp.API.Helpers
 {
     public static class Extensions
@@ -10,5 +10,17 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
+
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if(theDateTime.AddYears(age) > DateTime.Today)
+            {
+                age--;
+            }
+            return age;
+        }
     }
+
+ 
 }
